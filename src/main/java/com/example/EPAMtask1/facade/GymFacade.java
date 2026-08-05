4fb4e2af4543e8cc7c2bc1d2e314b79d4574ba21
Daniil Radevich <98713316+DanilRein@users.noreply.github.com>
2026-08-05 19:23:09 +0200

    Rewrote GymFacade for current hibernate usage

--- src/main/java/com/example/EPAMtask1/facade/GymFacade.java
+++ src/main/java/com/example/EPAMtask1/facade/GymFacade.java
@@ -7,10 +7,10 @@ import com.example.EPAMtask1.model.TrainingType;
 import com.example.EPAMtask1.services.TraineeService;
 import com.example.EPAMtask1.services.TrainerService;
 import com.example.EPAMtask1.services.TrainingService;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Component
 public class GymFacade {
@@ -23,33 +23,63 @@ public class GymFacade {
         this.trainingService = trainingService;
     }
 
+    //Trainee methods
+
     public Trainee createTrainee(String firstName, String lastName, LocalDate dateOfBirth, String address) {
         return traineeService.createTrainee(firstName, lastName, dateOfBirth, address);
     }
-    public void updateTrainee(int id, String firstName, String lastName, LocalDate dateOfBirth, String address) {
-        traineeService.updateTrainee(id, firstName, lastName, dateOfBirth, address);
+    public void updateTrainee(String authUsername, String authPassword, int id, String firstName, String lastName, LocalDate dateOfBirth, String address) {
+        traineeService.updateTrainee(authUsername, authPassword, id, firstName, lastName, dateOfBirth, address);
     }
-    public void deleteTrainee(int id) {
-        traineeService.deleteTrainee(id);
+    public void deleteTrainee(String authUsername, String authPassword) {
+        traineeService.deleteTrainee(authUsername, authPassword);
     }
-    public Trainee selectTrainee(int id) {
-        return traineeService.selectTrainee(id);
+    public Trainee selectTrainee(String authUsername, String authPassword, int id) {
+        return traineeService.selectTrainee(authUsername, authPassword, id);
     }
+    public List<Trainer> updateTraineeTrainers(String authUsername, String authPassword, int traineeId, List<Integer> trainerIds) {
+        return traineeService.updateTraineeTrainers(authUsername, authPassword, traineeId, trainerIds);
+    }
+    public void changeTraineePassword(String authUsername, String oldPassword, String newPassword) {
+        traineeService.changePassword(authUsername, oldPassword, newPassword);
+    }
+    public void toggleTraineeActiveStatus(String authUsername, String authPassword, int id) {
+        traineeService.toggleActiveStatus(authUsername, authPassword, id);
+    }
+
+    //Trainer methods
 
     public Trainer createTrainer(String firstName, String lastName, TrainingType specialization) {
         return trainerService.createTrainer(firstName, lastName, specialization);
     }
-    public void updateTrainer(int id, String firstName, String lastName, TrainingType specialization) {
-        trainerService.updateTrainer(id, firstName, lastName, specialization);
+    public void updateTrainer(String authUsername, String authPassword, int id, String firstName, String lastName, TrainingType specialization) {
+        trainerService.updateTrainer(authUsername, authPassword, id, firstName, lastName, specialization);
     }
-    public Trainer selectTrainer(int id) {
-        return trainerService.selectTrainer(id);
+    public Trainer selectTrainer(String authUsername, String authPassword, int id) {
+        return trainerService.selectTrainer(authUsername, authPassword, id);
+    }
+    public List<Trainer> findUnassignedTrainers(String authUsername, String authPassword, String traineeUsername) {
+        return trainerService.findUnassignedTrainers(authUsername, authPassword, traineeUsername);
+    }
+    public void changeTrainerPassword(String authUsername, String oldPassword, String newPassword) {
+        trainerService.changePassword(authUsername, oldPassword, newPassword);
+    }
+    public void toggleTrainerActiveStatus(String authUsername, String authPassword, int id) {
+        trainerService.toggleActiveStatus(authUsername, authPassword, id);
     }
 
-    public Training createTraining(int traineeId, int trainerId, String trainingName, TrainingType trainingType, LocalDate trainingDate, int trainingDuration) {
-        return trainingService.createTraining(traineeId, trainerId, trainingName, trainingType, trainingDate, trainingDuration);
+    //Training methods
+
+    public Training createTraining(String authUsername, String authPassword, int traineeId, int trainerId, String trainingName, TrainingType trainingType, LocalDate trainingDate, int trainingDuration) {
+        return trainingService.createTraining(authUsername, authPassword, traineeId, trainerId, trainingName, trainingType, trainingDate, trainingDuration);
     }
-    public Training selectTraining(int trainingId) {
-        return trainingService.selectTraining(trainingId);
+    public Training selectTraining(String authUsername, String authPassword, int trainingId) {
+        return trainingService.selectTraining(authUsername, authPassword, trainingId);
+    }
+    public List<Training> getTraineeTrainingsByCriteria(String authUsername, String authPassword, String traineeUsername, LocalDate startDate, LocalDate endDate, String trainerName, String trainingTypeName) {
+        return trainingService.getTraineeTrainingsByCriteria(authUsername, authPassword, traineeUsername, startDate, endDate, trainerName, trainingTypeName);
+    }
+    public List<Training> getTrainerTrainingsByCriteria(String authUsername, String authPassword, String trainerUsername, LocalDate startDate, LocalDate endDate, String traineeName) {
+        return trainingService.getTrainerTrainingsByCriteria(authUsername, authPassword, trainerUsername, startDate, endDate, traineeName);
     }
 }
